cc79ab03710a875bb193977ba315113429f6c455
Dongyuan-Ni <45381254+Dongyuan-Ni@users.noreply.github.com>
2023-01-03 12:19:32 +0800

    Create findnode.py

---
+++ findnode.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+
+gap_tol = float(input('Input the gap tolerance:'))
+Ef = float(os.popen('grep E-fermi OUTCAR').read().split()[2])
+
+with open('IBZKPT', 'r') as f:
+	f.readline()
+	nkpts_scf = int(f.readline())
+
+data = []
+
+with open('EIGENVAL', 'r') as f:
+	for i in range(5):
+		f.readline()
+	number = [int(i) for i in f.readline().split()]
+	nelectrons = number[0]
+	noccupancy = int(nelectrons / 2)
+	nkpts = number[1]
+	nbnds = number[2]
+	f.readline()
+	for i in range(nkpts_scf):
+		for j in range(nbnds+2):
+			f.readline()
+
+	for i in range(nkpts - nkpts_scf):
+		kpoint_coord = '    '.join(f.readline().split()[:3])
+		for j in range(noccupancy - 1):
+			f.readline()
+		Evb1 = float(f.readline().split()[1]) - Ef
+		Ecb1 = float(f.readline().split()[1]) - Ef
+		gap = Ecb1 - Evb1
+		if gap < gap_tol:
+			data.append(kpoint_coord + ' ' + str(gap) + '\n')	
+
+		for j in range(nbnds - noccupancy - 1):
+			f.readline()
+		if i != nkpts - nkpts_scf - 1:
+			f.readline()
+		else:
+			pass
+
+with open('k-positions.dat', 'w') as f:
+	f.writelines(data)
